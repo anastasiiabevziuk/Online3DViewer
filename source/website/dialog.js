292b318038a434bc1f29f614ce6ced82279704d4
Anastasiia Bevziuk <anastasiiabevziuk@gmail.com>
2024-09-04 15:36:55 +0300

    feat: progress img

--- source/website/dialog.js
+++ source/website/dialog.js
@@ -142,7 +142,7 @@ export class ProgressDialog extends Dialog
         let contentDiv = this.GetContentDiv ();
         contentDiv.classList.add ('ov_progress');
 
-        AddDiv (contentDiv, 'ov_progress_img', '<svg><use href="assets/images/3dviewer_net_logo.svg#logo"></use></svg>');
+        AddDiv (contentDiv, 'ov_progress_img', '<img  src="assets/images/3dviewer_logo.png" alt="logo" />');
         this.textDiv = AddDiv (contentDiv, 'ov_progress_text');
         this.SetText (text);
     }
